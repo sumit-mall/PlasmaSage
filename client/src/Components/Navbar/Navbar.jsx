4f92ee6f4323e26e3ca0a14724c34f05730719d1
saumyasharma12 <139276996+saumyasharma12@users.noreply.github.com>
2024-06-04 04:39:16 -0700

    auth use

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,34 +1,77 @@
-import React from 'react'
-import './Navbar.css'
-import logo_light from '../../assets/logo-black.png'
-import logo_dark from '../../assets/logo-white.png'
-import toggle_light from '../../assets/night.png'
-import toggle_dark from '../../assets/day.png'
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Navbar.css";
+import logo_light from "../../assets/logo-black.png";
+import logo_dark from "../../assets/logo-white.png";
+import toggle_light from "../../assets/night.png";
+import toggle_dark from "../../assets/day.png";
+import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const Navbar = ({theme,setTheme}) => {
+const Navbar = ({ theme, setTheme }) => {
+  const {loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
 
-    const toggle_mode = ()=> {
-        theme == 'light' ? setTheme('dark') : setTheme('light');
-    }
+  const toggle_mode = () => {
+    theme == "light" ? setTheme("dark") : setTheme("light");
+  };
 
   return (
     <div>
-        <div className="navbar">
-        <img src={theme == 'light' ? logo_light : logo_dark } alt="" className='logo' />
-        <div className="toggleMenu" onClick={()=>{toggleMenu()}}></div>
-        
-        <a href='/'><li className="nav-home">Home</li></a>
-        <a href='/donate'><li className="nav-donate">Donate</li></a>
-        <a href='/request'><li className="nav-request">Request</li></a>
-        <a href='/bloodbank'><li className="nav-bloodbank">BloodBank</li></a>
-        <a href='/about'><li className="nav-about">About</li></a>
-        
+      <div className="navbar">
+        <img
+          src={theme == "light" ? logo_light : logo_dark}
+          alt=""
+          className="logo"
+        />
+        <div
+          className="toggleMenu"
+          onClick={() => {
+            toggleMenu();
+          }}
+        ></div>
 
-        <img onClick={()=>{toggle_mode()}} src={theme == 'light' ? toggle_light : toggle_dark} alt="" className='toggle-icon'/>
-        </div>
+        <a href="/">
+          <li className="nav-home">Home</li>
+        </a>
+        <a href="/donate">
+          <li className="nav-donate">Donate</li>
+        </a>
+        <a href="/request">
+          <li className="nav-request">Request</li>
+        </a>
+        <a href="/bloodbank">
+          <li className="nav-bloodbank">BloodBank</li>
+        </a>
+        <a href="/about">
+          <li className="nav-about">About</li>
+        </a>
+        <li>{isAuthenticated && <p>{user.name}</p>}</li>
+        {isAuthenticated ? (
+          <li>
+            <button
+              onClick={() =>
+                logout({ logoutParams: { returnTo: window.location.origin } })
+              }
+            >
+              Log Out
+            </button>
+          </li>
+        ) : (
+          <li>
+            <button onClick={() => loginWithRedirect()}>Log In</button>
+          </li>
+        )}
+
+        <img
+          onClick={() => {
+            toggle_mode();
+          }}
+          src={theme == "light" ? toggle_light : toggle_dark}
+          alt=""
+          className="toggle-icon"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <Auth0Provider
+    domain="dev-d1ylsfjzqmetg2fu.us.auth0.com"
+    clientId="vEUPH0IuALoxc5RhykXlMqn3wvHZoB1o"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+    </Auth0Provider>
   </React.StrictMode>,
 )
